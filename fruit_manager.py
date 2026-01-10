178b8b806acb5ec843fa623459e814917f24014a
BarroAli <ai.hydrolaboratory@gmail.com>
2026-01-10 15:31:53 +0000

    ajout de la fonctionnalité trésorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     afficher_inventaire(inventaire)
     
     recolter(inventaire, "bananes", 10)
-    inventaire, tresorerie= vendre(inventaire, "bananes", 5)
+    inventaire, tresorerie= vendre(inventaire, "bananes", 5, tresorerie)
     
     ecrire_inventaire(inventaire)
     ecrire_tresorerie(tresorerie)
