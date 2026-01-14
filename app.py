3a2ec69890b51efd4786393e136fbacff0196f11
BarroAli <ai.hydrolaboratory@gmail.com>
2026-01-14 12:15:48 +0000

    ajout de la fonction vendre

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ inventaire = ouvrir_inventaire()
 prix = ouvrir_prix()
 tresorerie = ouvrir_tresorerie()
 
+with st.sidebar:
+    st.header("Vendre des fruits")
+    fruit_vendre = st.selectbox("chsoisir un fruit", list(inventaire.keys()))
+    quantite_vendre = st.number_input("quantité à vendre", min_value=1, step=1)
+    
+    if st.button("Vendre"):
+        inventaire,tresorerie = vendre(inventaire, fruit_vendre, quantite_vendre, tresorerie, prix)
+        
+
 st.header(" 💰 Trésorerie")
 st.metric("Montant disponible", value=f"{tresorerie:.2f} $")
 
